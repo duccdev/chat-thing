a3451f7e7ced1f6409061fb8a362544dc19336a8
duccdev <usr.bin.ducc.2@gmail.com>
2024-06-19 09:12:21 +0300

    feat: use pointers for the server instead of copying the type

--- server/mod.go
+++ server/mod.go
@@ -6,17 +6,17 @@ import (
 	"net"
 )
 
-func CreateServer(addr string, port uint16) (types.Server, error) {
+func CreateServer(addr string, port uint16) (*types.Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", addr, port))
 	maxClients := 3
 	maxMessageSize := 4000
 	maxUsernameSize := 16
 
 	if err != nil {
-		return types.Server{}, err
+		return nil, err
 	}
 
-	return types.Server{
+	return &types.Server{
 		Listener:        listener,
 		Clients:         make([]types.Client, maxClients),
 		MaxMessageSize:  uint(maxMessageSize),
@@ -24,7 +24,7 @@ func CreateServer(addr string, port uint16) (types.Server, error) {
 	}, nil
 }
 
-func Broadcast(server types.Server, msg string) {
+func Broadcast(server *types.Server, msg string) {
 	for _, client := range server.Clients {
 		client.Connection.Write([]byte(msg))
 	}
